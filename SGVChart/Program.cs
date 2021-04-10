ae3196146f0470bac53e545061ec0ddd7d7bb8e9
Yaseer Arafat <yaseer,arafat@live.com>
2021-04-10 23:48:28 +0600

    Add Project

---
+++ SGVChart/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SGVChart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var svgTemplate = "<svg viewBox='-1 -1 2 2' style='transform: rotate(-90deg)'>{0}</svg>";
+            var values = new[] { 0.1, 0.65, 0.2 };
+            var labels = new[] { "Coral", "CornflowerBlue", "#00ab6b" };
+            var result = SVGCreator.CreateSVG(svgTemplate, values, labels);
+            Console.WriteLine(result);
+        }
+
+    }
+}
---
+++ SGVChart/SVGCreator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace SGVChart
+{
+    public class SVGCreator
+    {
+        public static string CreateSVG(string svgTemplate, double[] values, string[] labels)
+        {
+            var paths = GenerateSVGPieChart(values, labels);
+            return string.Format(svgTemplate, paths);
+        }
+        private static (double X, double Y) GetCoordinatesForPercent(double percent)
+        {
+            var x = Math.Cos(2 * Math.PI * percent);
+            var y = Math.Sin(2 * Math.PI * percent);
+            return (x, y);
+        }
+        private static string GenerateSVGPieChart(double[] values, string[] labels)
+        {
+            var sb = new StringBuilder();
+            var cumulativePercent = 0D;
+
+            foreach (var slice in labels.Zip(
+                values, (label, value) => new { Color = label, Prercent = value }))
+            {
+                // destructing assignment sets the two variables at once
+                var (startX, startY) = GetCoordinatesForPercent(cumulativePercent);
+
+                // each slice starts where the last slice ended, so keep a cumulative percent
+                cumulativePercent += slice.Prercent;
+                var (endX, endY) = GetCoordinatesForPercent(cumulativePercent);
+
+                // if the slice is more than 50%, take the large arc (the long way around)
+                var largeArcFlag = slice.Prercent > .5 ? 1 : 0;
+
+                // create an array and join it just for code readability
+                var pathData = string.Join(" ", new[] {
+                    $"M {startX} {startY}",// Move
+                    $"A 1 1 0 {largeArcFlag} 1 {endX} {endY}",// Arc
+                    "L 0 0" // Line
+                });
+
+                sb.Append($"<path d='{pathData}' fill='{slice.Color}'></path>");
+            }
+            return sb.ToString();
+        }
+    }
+}
